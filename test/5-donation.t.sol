77000e17f1c40d4ef40a2116307f798ef4f38ddf
eugenioclrc <eugenioclrc@gmail.com>
2022-08-09 12:51:09 -0300

    add challenge 5

---
+++ test/5-donation.t.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "forge-std/console2.sol";
+
+interface IDonation {
+    function donate(uint256 etherAmount) external payable;
+    function withdraw() external;
+    function isComplete() external view returns (bool);
+}
+
+contract Challenge5Test is Test {
+    IDonation target;
+    address player = vm.addr(1);
+
+    function setUp() public {
+        address deployment = deployCode("5-Donation.sol:DonationChallenge", 1 ether);
+        target = IDonation(deployment);
+        vm.label(address(target), "Challenge");
+        vm.label(player, "Player");
+        vm.deal(player, 1 ether);
+    }
+
+    function testComplete() external {
+        vm.startPrank(player);
+
+        // tu codigo aca
+
+        assertTrue(target.isComplete(), "Challenge should be complete");
+    }
+}
